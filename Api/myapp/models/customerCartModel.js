c2214fd5fb48ec45d0833599b2d02805b52dae3d
InfinixPryer <akdfilipino@gmail.com>
2021-02-18 23:18:25 +0800

    Updated CustomerCart and CustomerCartModel

--- Api/myapp/models/customerCartModel.js
+++ Api/myapp/models/customerCartModel.js
@@ -5,7 +5,11 @@ const CustomerCartSchema = new mongoose.Schema({
     cartId: { type: String, unique: true},
     orderIds: [{ type: mongoose.Schema.Types.ObjectId , ref:"OrderItem", required: true}],
     totalPrice: { type: Number },
-    approved:{ type: Boolean, default: false}
+    approved:{ type: Boolean, default: false},
+    cusName:{ type: String, required: true},
+    cusAddress: { type:String, required: true},
+    cusPhone: { type:String, required: true, min:10},
+    cusEmail:{ type:String, required:true}
 });
 
 module.exports = mongoose.model('CustomerCart', CustomerCartSchema);
