e5f10a0f987cefc9e9aae92901bfdfbd6fd0f707
Meph27 <61321282+Meph27@users.noreply.github.com>
2021-02-20 16:03:37 +0800

    crashing-error-retry

--- Api/myapp/server.js
+++ Api/myapp/server.js
@@ -1,8 +1,8 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
 const port = process.env.PORT || 9000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log(`Server running at ${port}`);
+  console.log(`Server running at ${port}`);
 });
