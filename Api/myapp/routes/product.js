c1b70cbde764b7f9aa742b6ba95548f0d701bf03
Meph27 <61321282+Meph27@users.noreply.github.com>
2021-02-24 18:56:55 +0800

    Update product.js

--- Api/myapp/routes/product.js
+++ Api/myapp/routes/product.js
@@ -1,8 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // const multer = require('multer');
-
 
 // const storage = multer.diskStorage({
 //     destination: function(req, file, cb){
@@ -28,177 +27,176 @@ const mongoose = require('mongoose');
 //     fileFilter: fileFilter
 //     });
 
-const Product = require('../models/productModel');
+const Product = require("../models/productModel");
 
-router.get('/', (req, res, next) => {
-    Product.find()
-    .select('-__v -_id -options._id')
+router.get("/", (req, res, next) => {
+  Product.find()
+    .select("-__v -_id -options._id")
     .exec()
-    .then(docs => {
-        const response = {
-            count: docs.length,
-            products: docs,
-            request: {
-                type: 'GET'
-            }
-        }
-        res.status(200).json(response);
+    .then((docs) => {
+      const response = {
+        count: docs.length,
+        products: docs,
+        request: {
+          type: "GET",
+        },
+      };
+      res.status(200).json(response);
     })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        });
-    });
-    
-});
-
-router.get('/tags', (req, res, next) => {
-    Product.find()
-    .select('tags -_id')
-    .exec()
-    .then(productTags => {
-        var result = [];
-        var tagArrays = [];
-        productTags.forEach(tags => {
-            tagArrays.push(tags.tags)
-        })
-        tagArrays.forEach(tagArray => {
-            tagArray.forEach(tags => {
-                if(!result.includes(tags))
-                {
-                    console.log(tags);
-                    result.push(tags);
-                }
-            })
-        });
-
-        res.status(200).json(result)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({err})
-    })
-});
-
-router.get('/:productName', (req, res, next) => {
-    const name = req.params.productName;
-    Product.find({productName:name})
-    .select('-__v -_id -options._id')
-    .exec()
-    .then( doc => {
-        if(doc.length > 0){
-            res.status(200).json({
-                product: doc[0],
-                request: {
-                    type: 'GET'
-                }
-            })
-        }
-    })
-    .catch(error => {
-        res.status(500).json({
-            error
-        })
-    })
-
-    next()
-})
-
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    Product.find({productId:id})
-    .select('-__v -_id -options._id')
-    .exec()
-    .then( doc => {
-        if(doc.length > 0){
-            res.status(200).json({
-                product: doc[0],
-                request: {
-                    type: 'GET'
-                }
-            })
-        }
-    })
-    .catch(error => {
-        res.status(500).json({
-            error
-        })
-    })
-})
-
-
-
-router.post('/'/*, upload.single('productImage')*/, (req, res, next) => {
-    console.log(req.file);
-    const product = new Product({
-        productId: req.body.productId,
-        productName: req.body.productName,
-        productImage: req.body.productImage,
-        availability: req.body.availability,
-        type: req.body.type,
-        details: req.body.details,
-        options: req.body.options,
-        preferences: req.body.preferences,
-        bundleItems: req.body.bundleItems,
-        tags: req.body.tags
-    });
-    product.save()
-    .then(result => {
-        res.status(201).json({
-            message: "Product saved successfully!",
-            createdProduct:{
-                productId: req.body.productId,
-                productName: req.body.productName,
-                price: req.body.price,
-                productImage: req.body.productImage,
-                availability: req.body.availability,
-                type: req.body.type,
-                details: req.body.details,
-                options: req.body.options,
-                preferences: req.body.preferences,
-                bundleItems: req.body.bundleItems,
-                tags: req.body.tags   
-            }
-        });
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error:err
-        });
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
     });
 });
 
-router.patch('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    // const updateOps = {};
-    // for(const ops of req.body){
-    //     updateOps[ops.propName] = ops.value;
-    // }
-    const updateBody = req.body;
-    Product.updateOne({productId:id}, {$set: updateBody})
+router.get("/tags", (req, res, next) => {
+  Product.find()
+    .select("tags -_id")
     .exec()
-    .then(result => {
-        res.status(200).json(result);
+    .then((productTags) => {
+      var result = [];
+      var tagArrays = [];
+      productTags.forEach((tags) => {
+        tagArrays.push(tags.tags);
+      });
+      tagArrays.forEach((tagArray) => {
+        tagArray.forEach((tags) => {
+          if (!result.includes(tags)) {
+            console.log(tags);
+            result.push(tags);
+          }
+        });
+      });
+
+      res.status(200).json(result);
     })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ err });
+    });
+});
+
+router.get("/:productName", (req, res, next) => {
+  const name = req.params.productName;
+  Product.find({ productName: name })
+    .select("-__v -_id -options._id")
+    .exec()
+    .then((doc) => {
+      if (doc.length > 0) {
+        res.status(200).json({
+          product: doc[0],
+          request: {
+            type: "GET",
+          },
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error,
+      });
+    });
+
+  next();
+});
+
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.find({ productId: id })
+    .select("-__v -_id -options._id")
+    .exec()
+    .then((doc) => {
+      if (doc.length > 0) {
+        res.status(200).json({
+          product: doc[0],
+          request: {
+            type: "GET",
+          },
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error,
+      });
+    });
+});
+
+router.post("/" /*, upload.single('productImage')*/, (req, res, next) => {
+  console.log(req.file);
+  const product = new Product({
+    productId: req.body.productId,
+    productName: req.body.productName,
+    productImage: req.body.productImage,
+    availability: req.body.availability,
+    type: req.body.type,
+    details: req.body.details,
+    options: req.body.options,
+    preferences: req.body.preferences,
+    bundleItems: req.body.bundleItems,
+    tags: req.body.tags,
+  });
+  product
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Product saved successfully!",
+        createdProduct: {
+          productId: req.body.productId,
+          productName: req.body.productName,
+          price: req.body.price,
+          productImage: req.body.productImage,
+          availability: req.body.availability,
+          type: req.body.type,
+          details: req.body.details,
+          options: req.body.options,
+          preferences: req.body.preferences,
+          bundleItems: req.body.bundleItems,
+          tags: req.body.tags,
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.patch("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  // const updateOps = {};
+  // for(const ops of req.body){
+  //     updateOps[ops.propName] = ops.value;
+  // }
+  const updateBody = req.body;
+  Product.updateOne({ productId: id }, { $set: updateBody })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
     });
 });
 
 //TEST: Deletes a single product using productID
-router.delete('/:productId', (req, res, next)=>{
-    const id = req.params.productId;
-    Product.deleteOne({ productId: id})
+router.delete("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.deleteOne({ productId: id })
     .exec()
-    .then(result => {
-        res.status(200).json(result)
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
 });
 
