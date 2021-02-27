ce36ffde90327c2d31d1152c175915ee833aa317
InfinixPryer <akdfilipino@gmail.com>
2021-02-27 08:07:58 +0800

    Fixed a something

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, '/Client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.js'))
+    res.sendFile(path.join(__dirname, 'Client', 'build', 'index.js'))
   })
 }else{
   app.get('/', (req, res) => {
