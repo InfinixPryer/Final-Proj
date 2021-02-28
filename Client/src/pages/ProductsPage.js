214579d832208a1df3f978eb793a54b04e6ec108
Meph27 <61321282+Meph27@users.noreply.github.com>
2021-02-28 22:26:29 +0800

    noEslint-Warnings

--- Client/src/pages/ProductsPage.js
+++ Client/src/pages/ProductsPage.js
@@ -94,7 +94,6 @@ const FinderBar = ({ setFind, setCateg, int_state }) => {
                 rel="noreferrer"
                 href="https://www.facebook.com/phcoffeemonkey"
                 className="font-bold hover:text-coffee"
-                rel="noreferrer"
               >
                 {` Coffee Monkey Ph`}
               </a>{" "}
